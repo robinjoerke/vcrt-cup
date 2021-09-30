3fdb3814944766d48b546bb7bcbce1da96797d80
Robin Jörke <robin.joerke@solutiance.com>
2021-09-30 10:46:36 +0200

    restyling, restructuring, favicon, output structure

--- webpack.config.js
+++ webpack.config.js
@@ -43,16 +43,27 @@ module.exports = {
                         },
                     },
                 ],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename: 'resources/[hash][ext][query]'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html')
+            template: path.join(__dirname, 'src', 'index.html'),
+            favicon: "./icon/icon512x512.png"
         }),
 
         new MiniCssExtractPlugin({
